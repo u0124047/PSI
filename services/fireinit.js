6dc4fc56a27f837822cce9370b5d56d87905b34d
dwm331 <t9618006@ntut.org.tw>
2021-09-07 11:03:37 +0800

    移除不必要的參數

--- services/fireinit.js
+++ services/fireinit.js
@@ -3,14 +3,8 @@ import 'firebase/auth'
 import 'firebase/database'
 
 let config = {
-  apiKey: "AIzaSyAsy6VJQt_jJZNuozgH1S87F1iOMr3IGgE",
-  authDomain: "purchasesalesinventory.firebaseapp.com",
+  apiKey: "AIzaSyDLrS-F2MUDWLd8wLrSQl7yjdNtg5iaPa8",
   databaseURL: "https://purchasesalesinventory-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "purchasesalesinventory",
-  storageBucket: "purchasesalesinventory.appspot.com",
-  messagingSenderId: "271310288542",
-  appId: "1:271310288542:web:e3b2000a5ec7c76656ed8f",
-  measurementId: "G-5XR1MG10V8"
 }
 
 !firebase.apps.length ? firebase.initializeApp(config) : ''
