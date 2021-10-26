f9807d26fb072a787ebbd4b550469a433834a5c6
Kathy Kuo <dudoo.kathy@gmail.com>
2021-10-26 10:19:34 +0800

    new SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PSI',
+    title: '展虹日記帳',
     htmlAttrs: {
       lang: 'en'
     },
@@ -20,7 +20,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: '展虹有限公司｜日記帳', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
